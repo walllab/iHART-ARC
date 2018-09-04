8bdc117fc0174698413e9130d54822fe1ea48c97
saarteag2 <40413044+saarteag2@users.noreply.github.com>
2018-09-04 14:28:30 -0700

    Remove lab specific paths

--- Scripts/setAnnotationEnv.sh
+++ Scripts/setAnnotationEnv.sh
@@ -9,20 +9,14 @@ echo2() { printf "%s\n" "$*" >&2; }; #Print string to STDERR
 
 #--- Directories, Tools & Files -----------------------------------
 
-MAIN="/ifs/collabs/geschwind"
-IHART="$MAIN/NGS/NGS_analyses/iHART_genomes"
-PROG="$MAIN/programs"
-RDNV="$IHART/RDNVs"
-ML="$RDNV/machineLearning_to_distinguish_LCLartifacts"
+IG_LIST="" #Absolute path to Human_immunoglobulin_genes_HGNClist.txt file provided in the Annotation_Source_Files directory
+GIAB="" #Absolute path to Genome_in_a_bottle_problematic_regions.bed file provided in the Annotation_Source_Files directory
 
-IG_LIST="$IHART/July2015_Analysis/Human_immunoglobulin_genes_HGNClist.txt" #Absolute path to Human_immunoglobulin_genes_HGNClist.txt file provided in the Annotation_Source_Files directory
-GIAB="$MAIN/NGS/resources/Genome_in_a_bottle/Processed_Bed_Files/iHART_prob_genomic_regions_sorted_MERGED_REGIONS_sizes.bed" #Absolute path to Genome_in_a_bottle_problematic_regions.bed file provided in the Annotation_Source_Files directory
+ANNOVAR="" #Absolute path path to your preferred perl installation followed by an absolute path to the table_annovar.pl script from ANNOVAR
+ANV_DIR=""; #Absolute path to Annotation_Source_Files directory
+AWK="" #Absolute path to Gawk software directory
+BED="" #Absolute path to BEDTools toolset directory 
 
-ANNOVAR="/usr/local/ActivePerl-5.18.4.1804/bin/perl $PROG/annovar_2015DEC14/table_annovar.pl" #Absolute path path to your preferred perl installation followed by an absolute path to the table_annovar.pl script from ANNOVAR
-ANV_DIR="$ML/archive/humandb"; #Absolute path to Annotation_Source_Files directory
-AWK="$PROG/gawk-4.1.4/gawk" #Absolute path to Gawk software directory
-BED="$PROG/bedtools/bedtools2/bin/bedtools" #Absolute path to BEDTools toolset directory 
-
-REFG="$MAIN/NGS/reference_genome/hs37d5.fa" # Absolute path to Human Reference Genome hs37d5 file
+REFG="" # Absolute path to Human Reference Genome hs37d5 file
 
 source ${SRC}/data_config.sh
